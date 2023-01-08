6578502e6be9229f8fd6a6b258d54c9cc1cc3e70
Thomas.Ellyatt <thomas.ellyatt@gmail.com>
2023-01-08 20:30:59 +0000

    Updated line and font colours

--- main.py
+++ main.py
@@ -14,13 +14,15 @@ for index, row in df.iterrows():
         pdf.add_page()
 
         # Header Config
-        pdf.set_font(family="Poppins", style="B", size=14)
+        pdf.set_font(family="Poppins", style="B", size=16)
         pdf.set_text_color(19, 38, 47)
+        pdf.set_draw_color(254, 95, 85)
         pdf.set_line_width(1)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="L", ln=1, border=0)
         pdf.line(10, 20, 200, 20)
 
-        # Lined Sheet
+        # Line Config
+        pdf.set_draw_color(0, 0, 0)
         pdf.set_line_width(0.1)
         for line in range(30, 280, 10):
             pdf.line(10, line, 200, line)
@@ -30,7 +32,14 @@ for index, row in df.iterrows():
         pdf.line(10, 280, 200, 280)
         pdf.ln(260)
         pdf.set_font(family="Poppins", style="", size=8)
-        pdf.cell(w=0, h=8, txt=f"{row['Topic']} | Page {x + 1} of {row['Pages']}", align="R", ln=1, border=0)
+        pdf.cell(
+            w=0,
+            h=8,
+            txt=f"{row['Topic']} | Page {x + 1} of {row['Pages']}",
+            align="R",
+            ln=1,
+            border=0
+        )
 
 # Export PDF file
 pdf.output("output.pdf")
